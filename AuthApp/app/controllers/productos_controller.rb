7d1109a314d9e972e18e4e7ddb98fd58e3d46d88
guicho0601 <guicholarg@hotmail.com>
2013-07-19 09:25:27 -0600

    Agregando readme txt

---
+++ AuthApp/app/controllers/productos_controller.rb
@@ -0,0 +1,25 @@
+class ProductosController < ApplicationController
+
+  before_filter :require_login
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ AuthApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthApp::Application.config.secret_token = 'd14b48282c6b6de4f826015aa40996a3fb1cef2445153c74886f59714d260ea7fdc8c32da2d92aaa18cc0946ecca0f0c2f1cfb742c10743afb726946383b741d'
